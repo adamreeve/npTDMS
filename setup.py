192e22681e46c9b0cc00e0ee04490fba8eb58857
Adam Reeve <adreeve@gmail.com>
2012-03-08 11:20:19 +1300

    Add classifiers to setup.py

--- setup.py
+++ setup.py
@@ -11,4 +11,18 @@ setup(
   packages = ['nptdms', 'nptdms.test'],
   long_description=open('README.rst').read(),
   license = 'LGPL',
+  classifiers = [
+    'Development Status :: 4 - Beta',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.2',
+    'Topic :: Scientific/Engineering',
+    'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+    'Intended Audience :: Science/Research',
+    'Natural Language :: English',
+  ],
   install_requires = ['numpy'])
