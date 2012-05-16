f163d124a61e83c446440f85030fd41b0dda976c
Adam Reeve <adreeve@gmail.com>
2012-05-16 19:32:38 +1200

    Bump version to 0.2.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
   name = 'npTDMS',
-  version = '0.2.2',
+  version = '0.2.3',
   description = ("Cross-platform, NumPy based module for reading "
     "TDMS files produced by LabView."),
   author = 'Adam Reeve',
