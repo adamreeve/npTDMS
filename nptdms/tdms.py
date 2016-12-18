d7f1573bd759231bc467bbd3c7a57429cf40730f
Glenn Wikle <g.wikle@sigmalabsinc.com>
2016-12-18 16:46:33 -0700

    remove too-narrow property name checks

--- nptdms/tdms.py
+++ nptdms/tdms.py
@@ -853,8 +853,7 @@ class _TdmsmxDAQInfo(object):
         'scaler_data_type_code', 'scaler_data_type',
         'raw_buffer_index', 'raw_buffer_index', 'raw_byte_offset',
         'sample_format_bitmap', 'scale_id',
-        'raw_data_widths', 'properties',
-        '_property_names', '_values_check',
+        'raw_data_widths', 'properties'
         ]
 
     def info(self):
@@ -918,9 +917,6 @@ class _TdmsmxDAQInfo(object):
 
         num_properties = _read_long(f)
         self.properties = [None] * num_properties
-
-        self._property_names = self.__property_names[str(num_properties)]
-        self._values_check = self.__values_check[str(num_properties)]
 
         for cnt in range(num_properties):
             t_prop = _TdmsmxDAQPropertyInfo()
