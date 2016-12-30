13f4ba2e1e91ac09f626728cbd38cab23a2badc5
Adam Reeve <adreeve@gmail.com>
2016-12-30 11:40:38 +0000

    More acceptance tests for TDMS writing

--- nptdms/test/writer/acceptance_tests.py
+++ nptdms/test/writer/acceptance_tests.py
@@ -4,6 +4,8 @@ from datetime import datetime
 from io import BytesIO
 import logging
 import numpy as np
+import os
+import tempfile
 import unittest
 try:
     import pytz
@@ -11,6 +13,7 @@ except ImportError:
     pytz = None
 
 from nptdms import tdms, writer
+from nptdms import TdmsFile, TdmsWriter, RootObject, GroupObject, ChannelObject
 
 
 class TDMSTestClass(unittest.TestCase):
@@ -22,15 +25,15 @@ class TDMSTestClass(unittest.TestCase):
         a_input = np.linspace(0.0, 1.0, 100)
         b_input = np.linspace(0.0, 100.0, 100)
 
-        a_segment = writer.ChannelObject("group", "a", a_input)
-        b_segment = writer.ChannelObject("group", "b", b_input)
+        a_segment = ChannelObject("group", "a", a_input)
+        b_segment = ChannelObject("group", "b", b_input)
 
         output_file = BytesIO()
-        with writer.TdmsWriter(output_file) as tdms_writer:
+        with TdmsWriter(output_file) as tdms_writer:
             tdms_writer.write_segment([a_segment, b_segment])
 
         output_file.seek(0)
-        tdms_file = tdms.TdmsFile(output_file)
+        tdms_file = TdmsFile(output_file)
 
         a_output = tdms_file.object("group", "a").data
         b_output = tdms_file.object("group", "b").data
@@ -45,21 +48,21 @@ class TDMSTestClass(unittest.TestCase):
         if pytz:
             test_time = test_time.replace(tzinfo=pytz.utc)
 
-        a_segment = writer.RootObject(properties={
+        a_segment = RootObject(properties={
             "prop1": "foo",
             "prop2": 3,
         })
-        b_segment = writer.GroupObject("group_name", properties={
+        b_segment = GroupObject("group_name", properties={
             "prop3": 1.2345,
             "prop4": test_time,
         })
 
         output_file = BytesIO()
-        with writer.TdmsWriter(output_file) as tdms_writer:
+        with TdmsWriter(output_file) as tdms_writer:
             tdms_writer.write_segment([a_segment, b_segment])
 
         output_file.seek(0)
-        tdms_file = tdms.TdmsFile(output_file)
+        tdms_file = TdmsFile(output_file)
 
         a_output = tdms_file.object()
         b_output = tdms_file.object("group_name")
@@ -72,3 +75,46 @@ class TDMSTestClass(unittest.TestCase):
         self.assertEqual(a_output.properties["prop2"], 3)
         self.assertEqual(b_output.properties["prop3"], 1.2345)
         self.assertEqual(b_output.properties["prop4"], test_time)
+
+    def test_can_write_multiple_segments(self):
+        input_1 = np.linspace(0.0, 1.0, 10)
+        input_2 = np.linspace(2.0, 3.0, 10)
+
+        segment_1 = ChannelObject("group", "a", input_1)
+        segment_2 = ChannelObject("group", "a", input_2)
+
+        output_file = BytesIO()
+        with TdmsWriter(output_file) as tdms_writer:
+            tdms_writer.write_segment([segment_1])
+            tdms_writer.write_segment([segment_2])
+
+        output_file.seek(0)
+        tdms_file = TdmsFile(output_file)
+
+        output_data = tdms_file.object("group", "a").data
+
+        expected_data = np.append(input_1, input_2)
+        self.assertEqual(len(output_data), len(expected_data))
+        self.assertTrue((output_data == expected_data).all())
+
+    def test_can_write_to_file_using_path(self):
+        input_1 = np.linspace(0.0, 1.0, 10)
+        segment = ChannelObject("group", "a", input_1)
+
+        tempdir = tempfile.mkdtemp()
+        temppath = "%s/test_file.tdms" % tempdir
+        try:
+            with TdmsWriter(temppath) as tdms_writer:
+                tdms_writer.write_segment([segment])
+        finally:
+            if os.path.exists(temppath):
+                os.remove(temppath)
+            os.rmdir(tempdir)
+
+    def test_can_write_to_file_using_open_file(self):
+        input_1 = np.linspace(0.0, 1.0, 10)
+        segment = ChannelObject("group", "a", input_1)
+
+        with tempfile.NamedTemporaryFile(delete=True) as output_file:
+            with TdmsWriter(output_file.file) as tdms_writer:
+                tdms_writer.write_segment([segment])
