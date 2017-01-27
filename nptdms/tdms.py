3766115dbb48a7010478823aec91e9f9e7baf4fb
Adam Suban-Loewen <adam.subanloewen@gmail.com>
2017-01-26 23:27:37 -0500

    changed fromstring to frombuffer for improved performance

--- nptdms/tdms.py
+++ nptdms/tdms.py
@@ -93,14 +93,14 @@ else:
 
 def fromfile(file, dtype, count, *args, **kwargs):
     """Wrapper around np.fromfile to support any file-like object"""
-    
+
     if isinstance(file, int) or (
         hasattr(file, 'fileno') and callable(file.fileno)):
         # satisfy criteria that 'argument must be an int, or 
         # have a fileno() method' - from numpy TypeError
         return np.fromfile(file, dtype=dtype, count=count, *args, **kwargs)
     else:
-        return np.fromstring(
+        return np.frombuffer(
             file.read(count * np.dtype(dtype).itemsize),
             dtype=dtype, count=count, *args, **kwargs)
 
