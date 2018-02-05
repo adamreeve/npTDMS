fd496b32bc0c1e3dac52d6f9341bd1cf32cafa34
Gygger <jacob.rasco@gmail.com>
2018-02-04 23:17:01 -0800

    Test the value of the HDF5 dataset, not the object

--- nptdms/test/tdms_test.py
+++ nptdms/test/tdms_test.py
@@ -1002,8 +1002,8 @@ class TDMSTestClass(unittest.TestCase):
         h5 = tdmsData.as_hdf(h5_path)
         h5_strings = h5['Group']['String']
         self.assertEqual(h5_strings.dtype.kind, 'S')
-        self.assertEqual(len(h5_strings), len(strings))
-        for expected, read in zip(strings, h5_strings):
+        self.assertEqual(len(h5_strings.value), len(strings))
+        for expected, read in zip(strings, h5_strings.value):
             self.assertEqual(expected, read.astype(str))
         h5.close()
         os.remove(h5_path)
