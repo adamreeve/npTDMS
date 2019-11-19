583b5023e8dcb5a51298e7d25a54012392760c0c
Adam Reeve <adreeve@gmail.com>
2019-11-19 20:12:40 +0000

    Workaround for thermocouples-reference install in Python 3

--- run_tests.sh
+++ run_tests.sh
@@ -4,9 +4,10 @@ docker run -i -t --rm \
     -v $PWD:/nptdms:z \
     adreeve/python-numpy:latest \
     /bin/bash -c "
+        export LC_ALL=C.UTF-8 &&
         cd /nptdms &&
         pep8 ./nptdms &&
         pip install .[hdf,pandas,thermocouple_scaling] &&
         nosetests &&
-        pip3 install .[hdf,pandas] &&
+        pip3 install .[hdf,pandas,thermocouple_scaling] &&
         nosetests3"
