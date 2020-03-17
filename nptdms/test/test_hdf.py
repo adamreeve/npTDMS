787d667e7bb2293ae6296d51d0200a3260b1ba36
Adam Reeve <adreeve@gmail.com>
2020-03-17 15:05:44 +1300

    Add tests for conversion of different types to HDF

--- nptdms/test/test_hdf.py
+++ nptdms/test/test_hdf.py
@@ -1,6 +1,5 @@
 """ Test exporting TDMS data to HDF
 """
-import sys
 import pytest
 import numpy as np
 try:
@@ -11,7 +10,9 @@ except ImportError:
 from nptdms.test.util import (
     GeneratedFile,
     basic_segment,
+    channel_metadata,
     hexlify_value,
+    segment_objects_metadata,
     string_hexlify,
 )
 from nptdms.test import scenarios
@@ -30,6 +31,84 @@ def test_hdf_channel_data(tmp_path):
         h5_channel = h5[group][channel]
         assert h5_channel.dtype.kind == 'i'
         np.testing.assert_almost_equal(h5_channel[...], expected_data)
+    h5.close()
+
+
+def test_int_data_types(tmp_path):
+    """ Test conversion of signed and unsigned integer types to HDF
+    """
+    test_file = GeneratedFile()
+    test_file.add_segment(
+        ("kTocMetaData", "kTocRawData", "kTocNewObjList"),
+        segment_objects_metadata(
+            channel_metadata("/'group'/'i8'", 1, 4),
+            channel_metadata("/'group'/'u8'", 5, 4),
+            channel_metadata("/'group'/'i16'", 2, 4),
+            channel_metadata("/'group'/'u16'", 6, 4),
+            channel_metadata("/'group'/'i32'", 3, 4),
+            channel_metadata("/'group'/'u32'", 7, 4),
+            channel_metadata("/'group'/'i64'", 4, 4),
+            channel_metadata("/'group'/'u64'", 8, 4),
+        ),
+        "01 02 03 04"
+        "01 02 03 04"
+        "01 00 02 00 03 00 04 00"
+        "01 00 02 00 03 00 04 00"
+        "01 00 00 00 02 00 00 00 03 00 00 00 04 00 00 00"
+        "01 00 00 00 02 00 00 00 03 00 00 00 04 00 00 00"
+        "01 00 00 00 00 00 00 00 02 00 00 00 00 00 00 00 03 00 00 00 00 00 00 00 04 00 00 00 00 00 00 00"
+        "01 00 00 00 00 00 00 00 02 00 00 00 00 00 00 00 03 00 00 00 00 00 00 00 04 00 00 00 00 00 00 00"
+    )
+
+    tdms_data = test_file.load()
+    h5_path = tmp_path / 'h5_data_test.h5'
+    h5 = tdms_data.as_hdf(h5_path)
+
+    for chan, expected_dtype in [
+            ('i8', np.dtype('int8')),
+            ('u8', np.dtype('uint8')),
+            ('i16', np.dtype('int16')),
+            ('u16', np.dtype('uint16')),
+            ('i32', np.dtype('int32')),
+            ('u32', np.dtype('uint32')),
+            ('i64', np.dtype('int64')),
+            ('u64', np.dtype('uint64'))]:
+        h5_channel = h5['group'][chan]
+        assert h5_channel.dtype == expected_dtype
+        np.testing.assert_almost_equal(h5_channel[...], [1, 2, 3, 4])
+    h5.close()
+
+
+def test_floating_point_data_types(tmp_path):
+    """ Test conversion of f32 and f64 types to HDF
+    """
+    test_file = GeneratedFile()
+    test_file.add_segment(
+        ("kTocMetaData", "kTocRawData", "kTocNewObjList"),
+        segment_objects_metadata(
+            channel_metadata("/'group'/'f32'", 9, 4),
+            channel_metadata("/'group'/'f64'", 10, 4),
+        ),
+        hexlify_value('<f', 1) +
+        hexlify_value('<f', 2) +
+        hexlify_value('<f', 3) +
+        hexlify_value('<f', 4) +
+        hexlify_value('<d', 1) +
+        hexlify_value('<d', 2) +
+        hexlify_value('<d', 3) +
+        hexlify_value('<d', 4)
+    )
+
+    tdms_data = test_file.load()
+    h5_path = tmp_path / 'h5_data_test.h5'
+    h5 = tdms_data.as_hdf(h5_path)
+
+    for chan, expected_dtype in [
+            ('f32', np.dtype('float32')),
+            ('f64', np.dtype('float64'))]:
+        h5_channel = h5['group'][chan]
+        assert h5_channel.dtype == expected_dtype
+        np.testing.assert_almost_equal(h5_channel[...], [1.0, 2.0, 3.0, 4.0])
     h5.close()
 
 
