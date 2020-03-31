a12356ccfe79805a0f1f7a4cda38625a7eadde27
Adam Reeve <adreeve@gmail.com>
2020-03-31 22:59:42 +1300

    Add channel length to tdmsinfo output and improve test coverage (#175)
    * Add channel length to tdmsinfo output and improve test coverage
    * Test debug output in tdmsinfo

--- docs/tdmsinfo.rst
+++ docs/tdmsinfo.rst
@@ -8,22 +8,23 @@ Usage looks like::
     tdmsinfo [--properties] tdms_file
 
 Passing the ``--properties`` or ``-p`` argument will include TDMS object
-properties in the printed information.
+properties in the printed information as well as the data type and length of channels.
 
 The output of tdmsinfo including properties will look something like::
 
     /
-    properties:
-    name: test_file
-      /'group_1'
       properties:
-      group_property: property value
-        /'group_1'/'channel_a'
-          data type: tdsTypeU8
+        name: test_file
+      /'group_1'
+        properties:
+          group_property: property value
+        /'group_1'/'channel_1'
+          data type: Uint32
+          length: 2000
           properties:
-          wf_start_time: 2016-12-3014:56:00+00:00
-          wf_increment: 0.0005
-          wf_samples: 2000
+            wf_start_time: 2016-12-3014:56:00+00:00
+            wf_increment: 0.0005
+            wf_samples: 200
 
 There is also a ``--debug`` or ``-d`` argument that will output debug information
 to stderr, which can be useful when debugging a problem with a TDMS file.
--- nptdms/test/test_tdmsinfo.py
+++ nptdms/test/test_tdmsinfo.py
@@ -1,25 +1,52 @@
 import os
-from nptdms.tdmsinfo import tdmsinfo
+import sys
+from nptdms import tdmsinfo
 from nptdms.test.util import GeneratedFile, basic_segment
+try:
+    from unittest.mock import patch
+except ImportError:
+    from mock import patch
 
 
-def test_tdmsinfo():
+def test_tdmsinfo(capsys):
     test_file = GeneratedFile()
     test_file.add_segment(*basic_segment())
     temp_file = test_file.get_tempfile(delete=False)
     try:
         temp_file.file.close()
-        tdmsinfo(temp_file.name)
+        with patch.object(sys, 'argv', ['tdmsinfo.py', temp_file.name]):
+            tdmsinfo.main()
+            captured = capsys.readouterr()
+            assert "/'Group'/'Channel1'" in captured.out
+            assert "wf_start_offset" not in captured.out
     finally:
         os.remove(temp_file.name)
 
 
-def test_tdmsinfo_with_properties():
+def test_tdmsinfo_with_properties(capsys):
     test_file = GeneratedFile()
     test_file.add_segment(*basic_segment())
     temp_file = test_file.get_tempfile(delete=False)
     try:
         temp_file.file.close()
-        tdmsinfo(temp_file.name, show_properties=True)
+        with patch.object(sys, 'argv', ['tdmsinfo.py', temp_file.name, '--properties']):
+            tdmsinfo.main()
+            captured = capsys.readouterr()
+            assert "/'Group'/'Channel1'" in captured.out
+            assert "wf_start_offset: 0.0" in captured.out
+            assert "length: 2" in captured.out
+    finally:
+        os.remove(temp_file.name)
+
+
+def test_tdmsinfo_with_debug_output(caplog):
+    test_file = GeneratedFile()
+    test_file.add_segment(*basic_segment())
+    temp_file = test_file.get_tempfile(delete=False)
+    try:
+        temp_file.file.close()
+        with patch.object(sys, 'argv', ['tdmsinfo.py', temp_file.name, '--debug']):
+            tdmsinfo.main()
+            assert "Reading metadata for object /'Group'/'Channel1'" in caplog.text
     finally:
         os.remove(temp_file.name)
