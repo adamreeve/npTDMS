75b0896f3d9e90b4f12fb66413aafc790c7a8890
Adam Reeve <adreeve@gmail.com>
2020-05-12 10:10:58 +1200

    Create ndarray subclass for structured arrays of TDMS timestamps

--- nptdms/test/test_timestamps.py
+++ nptdms/test/test_timestamps.py
@@ -2,7 +2,9 @@
 """
 
 import numpy as np
+import pytest
 from nptdms import TdmsFile
+from nptdms.timestamp import TdmsTimestamp, TimestampArray
 from nptdms.test.util import (
     GeneratedFile,
     channel_metadata,
@@ -42,31 +44,108 @@ def test_read_raw_timestamp_data():
     """ Test reading timestamp data as a raw TDMS timestamps
     """
     test_file = GeneratedFile()
+    seconds = 3672033330
+    second_fractions = 1234567890 * 10 ** 10
     test_file.add_segment(
         ("kTocMetaData", "kTocRawData", "kTocNewObjList"),
         segment_objects_metadata(
             channel_metadata("/'group'/'channel1'", 0x44, 4),
         ),
-        hexlify_value("<Q", 0000000000) + hexlify_value("<q", 3672033330) +
-        hexlify_value("<Q", 1234567890) + hexlify_value("<q", 3672033330) +
-        hexlify_value("<Q", 0000000000) + hexlify_value("<q", 3672033331) +
-        hexlify_value("<Q", 1234567890) + hexlify_value("<q", 3672033331)
+        hexlify_value("<Q", 0) + hexlify_value("<q", seconds) +
+        hexlify_value("<Q", second_fractions) + hexlify_value("<q", seconds) +
+        hexlify_value("<Q", 0) + hexlify_value("<q", seconds + 1) +
+        hexlify_value("<Q", second_fractions) + hexlify_value("<q", seconds + 1)
     )
 
-    expected_seconds = np.array([3672033330, 3672033331, 3672033330, 3672033331], np.dtype('int64'))
-    expected_second_fractions = np.array([0, 2000000000, 0, 2000000000], np.dtype('uint64'))
-    expected_timestamps = np.array([
-        np.datetime64('2020-05-11 09:15:30'),
-        np.datetime64('2020-05-11 09:15:30'),
-        np.datetime64('2020-05-11 09:15:31'),
-        np.datetime64('2020-05-11 09:15:31'),
-    ])
+    expected_seconds = np.array([seconds, seconds, seconds + 1, seconds + 1], np.dtype('int64'))
+    expected_second_fractions = np.array([0, second_fractions, 0, second_fractions], np.dtype('uint64'))
 
     with test_file.get_tempfile() as temp_file:
         tdms_data = TdmsFile.read(temp_file.file, raw_timestamps=True)
         data = tdms_data['group']['channel1'][:]
+        assert isinstance(data, TimestampArray)
         np.testing.assert_equal(data['seconds'], expected_seconds)
         np.testing.assert_equal(data['second_fractions'], expected_second_fractions)
-        np.testing.assert_equal(data.as_datetime64(), expected_timestamps)
 
 
+def test_timestamp_array_slicing():
+    timestamp_array = _get_test_timestamp_array()
+    array_slice = timestamp_array[0:2]
+    assert isinstance(array_slice, TimestampArray)
+
+
+def test_timestamp_array_get_single_item():
+    timestamp_array = _get_test_timestamp_array()
+    array_item = timestamp_array[3]
+    assert isinstance(array_item, TdmsTimestamp)
+    assert array_item.seconds == 3672033331
+    assert array_item.second_fractions == 1234567890 * 10 ** 10
+
+
+def test_timestamp_array_field_access():
+    timestamp_array = _get_test_timestamp_array()
+    seconds = timestamp_array['seconds']
+    second_fractions = timestamp_array['second_fractions']
+    assert isinstance(seconds, np.ndarray)
+    assert not isinstance(seconds, TimestampArray)
+    assert seconds.dtype == np.dtype('<i8')
+    assert isinstance(second_fractions, np.ndarray)
+    assert not isinstance(second_fractions, TimestampArray)
+    assert second_fractions.dtype == np.dtype('<u8')
+
+
+def test_timestamp_array_to_datetime64():
+    timestamp_array = _get_test_timestamp_array()
+    expected_timestamps = np.array([
+        np.datetime64('2020-05-11 09:15:30'),
+        np.datetime64('2020-05-11 09:15:30.669260'),
+        np.datetime64('2020-05-11 09:15:31'),
+        np.datetime64('2020-05-11 09:15:31.669260'),
+    ])
+
+    us_array = timestamp_array.as_datetime64()
+
+    np.testing.assert_equal(us_array, expected_timestamps)
+
+
+def test_timestamp_array_to_datetime64_with_ns_precision():
+    timestamp_array = _get_test_timestamp_array()
+    expected_timestamps = np.array([
+        np.datetime64('2020-05-11 09:15:30'),
+        np.datetime64('2020-05-11 09:15:30.669260594'),
+        np.datetime64('2020-05-11 09:15:31'),
+        np.datetime64('2020-05-11 09:15:31.669260594'),
+    ])
+
+    ns_array = timestamp_array.as_datetime64('ns')
+
+    np.testing.assert_equal(ns_array, expected_timestamps)
+
+
+@pytest.mark.parametrize(
+    "input_array",
+    [
+        np.array([1, 2, 3, 4]),
+        np.array([(1, 2), (3, 4)], dtype=[('a', '<i8'), ('b', '<u8')]),
+        np.array([(1, 2), (3, 4)], dtype=[('seconds', '<i8'), ('b', '<u8')]),
+        np.array([(1, 2), (3, 4)], dtype=[('a', '<i8'), ('second_fractions', '<u8')]),
+    ]
+)
+def test_error_raised_with_creating_timestamp_array_with_invalid_input_type(input_array):
+
+    with pytest.raises(ValueError) as exc_info:
+        _ = TimestampArray(input_array)
+    assert str(exc_info.value) == "Input array must have a dtype with 'seconds' and 'second_fractions' fields"
+
+
+def _get_test_timestamp_array():
+    dtype = np.dtype([('seconds', '<i8'), ('second_fractions', '<u8')])
+    seconds = 3672033330
+    second_fractions = 1234567890 * 10 ** 10
+    array = np.array([
+        (seconds, 0),
+        (seconds, second_fractions),
+        (seconds + 1, 0),
+        (seconds + 1, second_fractions),
+    ], dtype=dtype)
+    return TimestampArray(array)
--- nptdms/timestamp.py
+++ nptdms/timestamp.py
@@ -14,24 +14,60 @@ class TdmsTimestamp(object):
         :ivar ~.second_fractions: A positive number of 2^-64 fractions of a second
     """
 
-    _fractions_per_step = {
-        's': 1.0 / 2 ** -64,
-        'ms': (10 ** -3) / 2 ** -64,
-        'us': (10 ** -6) / 2 ** -64,
-        'ns': (10 ** -9) / 2 ** -64,
-        'ps': (10 ** -12) / 2 ** -64,
-    }
-
     def __init__(self, seconds, second_fractions):
         self.seconds = seconds
         self.second_fractions = second_fractions
 
     def as_datetime64(self, resolution='us'):
         try:
-            fractions_per_step = self._fractions_per_step[resolution]
+            fractions_per_step = _fractions_per_step[resolution]
         except KeyError:
             raise ValueError("Unsupported resolution for converting to numpy datetime64: {0}".format(resolution))
         return (
                 EPOCH +
                 np.timedelta64(self.seconds, 's') +
                 ((self.second_fractions / fractions_per_step) * np.timedelta64(1, resolution)))
+
+
+class TimestampArray(np.ndarray):
+    """ A numpy array of TDMS timestamps
+
+        This must be constructed by passing a structured numpy array
+        with 'seconds' and 'second_fractions' fields.
+    """
+
+    def __new__(cls, input_array):
+        field_names = input_array.dtype.names
+        if field_names != ('seconds', 'second_fractions'):
+            raise ValueError("Input array must have a dtype with 'seconds' and 'second_fractions' fields")
+        return np.asarray(input_array).view(cls)
+
+    def __getitem__(self, item):
+        val = super(TimestampArray, self).__getitem__(item)
+        if isinstance(item, str):
+            # Getting a field, we don't want to return this as a TimestampArray
+            # but as a normal numpy ndarray
+            return val.view(np.ndarray)
+        if isinstance(item, int):
+            # Getting a single item
+            return TdmsTimestamp(val[0], val[1])
+        return val
+
+    def as_datetime64(self, resolution='us'):
+        try:
+            fractions_per_step = _fractions_per_step[resolution]
+        except KeyError:
+            raise ValueError("Unsupported resolution for converting to numpy datetime64: {0}".format(resolution))
+        return (
+                EPOCH +
+                self['seconds'] * np.timedelta64(1, 's') +
+                (self['second_fractions'] / fractions_per_step) * np.timedelta64(1, resolution))
+
+
+_fractions_per_step = {
+    's': 1.0 / 2 ** -64,
+    'ms': (10 ** -3) / 2 ** -64,
+    'us': (10 ** -6) / 2 ** -64,
+    'ns': (10 ** -9) / 2 ** -64,
+    'ps': (10 ** -12) / 2 ** -64,
+}
