ec4dd4d5c955254bbd0ec1c8f30a34463c44926c
Adam Reeve <adreeve@gmail.com>
2021-10-28 09:55:58 +1300

    Pin dependency versions used by readthedocs (#257)

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ sys.path.insert(0, os.path.abspath('..'))
 extensions = ["sphinx.ext.autodoc"]
 
 # Autodoc options
-autoclass_content = "doc"
+autoclass_content = "class"
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = []
