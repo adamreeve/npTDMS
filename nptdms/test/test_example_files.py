077a0091bf0cd13aac36986bfbf005b64950eb93
Jim Kring <jim.kring@jki.net>
2024-01-24 12:46:47 -0800

    refactor defragementation test - added tdms_files_assert_equal() support function that compares two TdmsFile instances - parametrized test_defragment() with multiple example files, for more test coverage

--- nptdms/test/test_example_files.py
+++ nptdms/test/test_example_files.py
@@ -1,11 +1,12 @@
-""" Test reading example TDMS files
-"""
+""" Test reading example TDMS files """
 
 import os
 from io import BytesIO
 
 import numpy as np
-from nptdms import tdms, TdmsWriter
+import pytest
+
+from nptdms import tdms, TdmsWriter, TdmsFile
 
 DATA_DIR = os.path.dirname(os.path.realpath(__file__)) + '/data'
 
@@ -34,26 +35,45 @@ def test_raw_format():
                                     0.2517777])
 
 
-def test_defragment_raw_timestamps():
-    """Test defragmenting a file with raw timestamps"""
-    test_file_path = DATA_DIR + '/raw_timestamps.tdms'
+def tdms_files_assert_equal(tdms1: TdmsFile, tdms2: TdmsFile):
+    """Assert that two TdmsFile instances are equal"""
+    # verify file properties
+    for p in tdms1.properties:
+        np.testing.assert_equal(tdms1.properties[p], tdms2.properties[p])
+    # verify group
+    for group in tdms1.groups():
+        # verify group properties
+        for p in group.properties:
+            np.testing.assert_equal(group.properties[p], tdms2[group.name].properties[p])
+        # verify channels
+        for channel in group.channels():
+            # verify channel data
+            np.testing.assert_equal(channel.data, tdms2[group.name][channel.name].data)
+            # verify channel properties
+            for p in channel.properties:
+                np.testing.assert_equal(channel.properties[p], tdms2[group.name][channel.name].properties[p])
+
+
+@pytest.mark.parametrize("tdms_file", [
+    'raw_timestamps.tdms',  # Test defragmentation of a file with raw timestamps
+    # 'raw1.tdms', # <- cannot defragment this file (ValueError: Channel data must be a 1d array)
+    'Digital_Input.tdms',
+    'big_endian.tdms',
+])
+def test_defragment(tdms_file):
+    """Test defragmentation round trip for a TDMS file"""
+    test_file_path = DATA_DIR + '/' + tdms_file
     output_file = BytesIO()
 
     # verify we can defragment a file with raw timestamps
     TdmsWriter.defragment(test_file_path, output_file)
 
-    # verify that both files' channel data are the same
-    input_tdms = tdms.TdmsFile(test_file_path)
+    # rewind output file BytesIO instance and read it back in as a TdmsFile
     output_file.seek(0)
     output_tdms = tdms.TdmsFile(output_file)
-    for group in input_tdms.groups():
-        for channel in group.channels():
-            # verify that both files' channel data are the same
-            np.testing.assert_equal(channel.data, output_tdms[group.name][channel.name].data)
-            # verify that both files' timestamps are the same
-            input_timestamps = channel.time_track()
-            output_timestamps = output_tdms[group.name][channel.name].time_track()
-            np.testing.assert_equal(input_timestamps, output_timestamps)
+
+    # verify that both TdmsFile objects are the same
+    tdms_files_assert_equal(tdms.TdmsFile(test_file_path), output_tdms)
 
 
 def test_big_endian_format():
